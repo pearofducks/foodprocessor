1215d2e0f5f668ee8a70f11d2a0bf196f591c7ae
Dave Honneffer <pearofducks@gmail.com>
2016-09-14 15:16:58 +0200

    Improve yamlToJSON a bit

--- yamlToJSON.cr
+++ yamlToJSON.cr
@@ -1,6 +1,12 @@
 require "yaml"
 require "json"
+if(ARGV.size == 0)
+  puts "\tyamlToJSON.cr INPUT_DIR [OUTPUT_DIR]"
+  puts "\t- if OUTPUT_DIR is omitted then STDOUT will be used"
+  exit(0)
+end
 files = Dir.glob(File.join(File.expand_path(ARGV[0]),"*.recipe"))
+puts files
 recipes = files.map do |f|
   begin
     YAML.parse(File.read(f)).as_h
@@ -8,4 +14,8 @@ recipes = files.map do |f|
     puts "ERROR at #{f}"
   end
 end
-File.write(File.join(File.expand_path(ARGV[1]),"recipes.js"), recipes.to_json)
+if(ARGV.size == 1)
+  puts recipes.to_json
+else
+  File.write(File.join(File.expand_path(ARGV[1]),"recipes.js"), recipes.to_json)
+end
